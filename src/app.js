09349e8cb7c0b1eb8dff7c67c1c8b7da466c7630
LaloEnri23 <126302462+LaloEnri23@users.noreply.github.com>
2023-05-11 10:08:55 -0700

    added all js files and css

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Portfolio from './Portfolio';
+import './style.css';
+
+function App() {
+    return (
+    <div className="App">
+        <header className="App-header">
+        <Portfolio />
+        </header>
+    </div>
+    );
+}
+
+export default App;
---
+++ src/portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+    <div>
+      {/* Paste your HTML code here */}
+    </div>
+    );
+}
+
+export default Portfolio;
